14a99ce796e7e336426208d547ba5655f3b39d0e
somuthink <qould12k@gmail.com>
2024-10-12 00:30:03 +0500

    was too lazy to commit (minimal tg bot with weather fetch and some inputs , without organized bot routes

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/somuthink/sirius_weather_bot
+
+go 1.22.1
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ internal/weather.go
@@ -0,0 +1,76 @@
+package internal
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var RequestBase = RequestBuilder()
+var ErrNotExistingCity = errors.New("1006")
+
+type Response struct {
+	Location struct {
+		Name string `json:"name"`
+	}
+	Current struct {
+		Temp_c float32 `json:"temp_c"`
+		Is_day int     `json:"is_day"`
+	}
+	Error struct {
+		Code int `json:"code"`
+	}
+}
+
+func RequestBuilder() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s", os.Getenv("WEATHER_TOKEN"))
+
+}
+
+func WeatherRequest(city string) (Response, error) {
+
+	var res Response
+
+	resp, err := http.Get(fmt.Sprintf("%s&q=%s", RequestBase, city))
+	if err != nil {
+		return res, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return res, err
+	}
+
+	if err := json.Unmarshal(body, &res); err != nil {
+		return res, err
+	}
+
+	return res, nil
+}
+
+func CheckCityExists(city string) (string, error) {
+	resp, err := WeatherRequest(city)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.Error.Code == 1006 {
+		return "", ErrNotExistingCity
+	}
+
+	return resp.Location.Name, nil
+
+}
